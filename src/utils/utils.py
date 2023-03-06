bcc77271423c05ba565a1b66b6ccea416ccc0311
Nikita <Shagalovprog@gmail.com>
2023-03-06 21:12:54 +0300

    Make error feedback in filler handler inputs

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -2,26 +2,6 @@ from src.utils.exceptions import UnexpectedErrorMessage
 from src.utils.models import DecoderErrorLocation
 
 
-# def parse_error_message(msg: str) -> DecoderErrorLocation:
-#     """line 2 column 1 (char 2)-F"""
-#     print(msg)
-#     print(msg[-4])
-#     print(msg[-12])
-#     print(msg[-12])
-#     error_location = DecoderErrorLocation(
-#         location="1. 'В который переносим ключи'" if msg[-1] == "T" else "2. 'Из которого переносим ключ'и",
-#         char=int(msg[-4]),
-#         column=int(msg[-12]),
-#         line=int(msg[-21])
-#     )
-#     try:
-#         return error_location
-#     except ValueError as err:
-#         raise UnexpectedErrorMessage(f"This message text was not expected: {msg}, can't handel")
-#
-# # print(parse_error_message("line 2 column 1 (char 2)-T"))
-
-
 def parse_error_message(msg: str):
     """Invalid control character at: line 13 column 44 (char 432)-T"""
 
